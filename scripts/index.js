c4a996c88f312a6824afa06a14eaa8d05d983edb
Mary Mart <marymart_ballet2016@mail.ru>
2020-11-16 16:11:51 +0300

    На проверку.

---
+++ scripts/index.js
@@ -0,0 +1,22 @@
+let popup = document.querySelector(".popup");
+let editButton = document.querySelector(".profile__edit-button");
+let popupClose = document.querySelector(".popup__close");
+let title = document.querySelector(".profile__name");
+let subtitle = document.querySelector(".profile__description");
+let form = document.querySelector(".popup__form");
+let nameField = document.querySelector(".popup__input_type_name");
+let infoField = document.querySelector(".popup__input_type_info");
+function showPopup() {
+  popup.classList.add("popup_opened");
+}
+function closePopup() {
+  popup.classList.remove("popup_opened");
+}
+editButton.addEventListener("click", showPopup);
+popupClose.addEventListener("click", closePopup);
+function submitForm(event) {
+  event.preventDefault();
+  title.textContent = nameField.value;
+  subtitle.textContent = infoField.value;
+}
+form.addEventListener("submit", submitForm);
